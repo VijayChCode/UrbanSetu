a3c0ba9ad9f348de044c04bc787bdd14640c3a05
Vijay <chalendravijay09@gmail.com>
2025-06-29 20:05:03 +0530

    fix: export app and analytics from firebase.js for Oauth import

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-b0d7e.firebaseapp.com",
-  projectId: "mern-estate-b0d7e",
-  storageBucket: "mern-estate-b0d7e.firebasestorage.app",
-  messagingSenderId: "406389541945",
-  appId: "1:406389541945:web:ae882489571b5b055c91e8"
+  apiKey: "AIzaSyCSIPavj9H3vKf_BfNHNL-l9BnbjgoGQcE",
+  authDomain: "urbansetu-76c8c.firebaseapp.com",
+  projectId: "urbansetu-76c8c",
+  storageBucket: "urbansetu-76c8c.firebasestorage.app",
+  messagingSenderId: "1085933309814",
+  appId: "1:1085933309814:web:39232deaaee1f69272b457",
+  measurementId: "G-M3EL8W29W9"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
