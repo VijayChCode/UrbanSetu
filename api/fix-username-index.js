141986f998c427c985555827c1b99bf0b03aa1cf
Vijay <chalendravijay09@gmail.com>
2025-07-01 22:47:56 +0530

    chore: update fix-username-index.js to use production DB URI for dropping username unique index

--- api/fix-username-index.js
+++ api/fix-username-index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 async function fixUsernameIndex() {
-  await mongoose.connect('mongodb://localhost:27017/mern-estate');
+  await mongoose.connect('mongodb+srv://Rajashekar:Rajashekar@mern-estate.kzrjh.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate&tls=true&tlsAllowInvalidCertificates=true');
   const collection = mongoose.connection.db.collection('users');
   try {
     const result = await collection.dropIndex('username_1');
