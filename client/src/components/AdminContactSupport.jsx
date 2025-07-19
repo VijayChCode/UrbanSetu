4d0b2a6907cc3e661066bfda65ceb137d4b898a5
Vijay <chalendravijay09@gmail.com>
2025-07-19 14:59:46 +0530

    homefix5

--- client/src/components/AdminContactSupport.jsx
+++ client/src/components/AdminContactSupport.jsx
@@ -60,6 +60,24 @@ export default function AdminContactSupport() {
     }
   }, [isModalOpen, isAdmin]);
 
+  // Scroll lock for modal
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
+    } else {
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+    };
+  }, [isModalOpen]);
+
   const fetchMessages = async () => {
     setLoading(true);
     setError("");
