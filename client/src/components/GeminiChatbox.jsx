4b224170142e4987fa50126bfc2c18570e6fcebd
VijayChCode <chalendravijay09@gmail.com>
2025-08-10 13:19:08 +0530

    Update GeminiChatbox.jsx

--- client/src/components/GeminiChatbox.jsx
+++ client/src/components/GeminiChatbox.jsx
@@ -176,7 +176,7 @@ const GeminiChatbox = () => {
                                         <div className="flex space-x-1">
                                             <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce"></div>
                                             <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{animationDelay: '0.1s'}}></div>
-                                            
+                                            <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{animationDelay: '0.2s'}}></div>
                                         </div>
                                     </div>
                                 </div>
