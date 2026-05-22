c34855863f892e52e159fa5d53cdda0c9df4eb1f
Vijay <chalendravijay09@gmail.com>
2026-05-22 12:07:46 +0530

    Bug: Signupfix01

--- web/src/pages/Profile.jsx
+++ web/src/pages/Profile.jsx
@@ -868,6 +868,8 @@ export default function Profile() {
   const handleProfileRecaptchaVerify = (token) => {
     setProfileRecaptchaToken(token);
     setProfileRecaptchaError("");
+    // Hide after a brief delay to show the tick
+    setTimeout(() => setShowProfileRecaptcha(false), 1000);
   };
 
   const handleProfileRecaptchaExpire = () => {
