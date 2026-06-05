792eebd7df100953f09b66de5f03a3e4151a2959
Vijay <chalendravijay09@gmail.com>
2026-06-05 09:38:14 +0530

    Fix missing useMemo import in RentWallet.jsx

--- web/src/pages/RentWallet.jsx
+++ web/src/pages/RentWallet.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { useNavigate, useLocation, Link, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { toast } from 'react-toastify';
